e5b17df8f557e95451b9bf7c0c2dcca918752121
Julia Lopez <jlopez@digitalorigin.com>
2016-03-16 23:27:23 +0100

    add new attribute via serializer

--- app/serializers/kitty_serializer.rb
+++ app/serializers/kitty_serializer.rb
@@ -1,7 +1,11 @@
 class KittySerializer < ActiveModel::Serializer
   attribute :name, key: :nickname
 
-  attributes :id, :birthday, :social
+  attributes :id, :birthday, :social, :name_with_prefix
+
+  def name_with_prefix
+    "kitty_#{object.name}"
+  end
 
   def social
     false
